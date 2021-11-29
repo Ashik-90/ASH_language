300be288488f138355abba5821d55a742ba29065
Yeasar <yeasar1507099@stud.kuet.ac.bd>
2021-11-29 23:25:57 +0600

    test

---
+++ Book.java
@@ -0,0 +1,10 @@
+public class Book {
+
+    private String title;
+    private String writer;
+
+    public Book(String title, String writer){
+        this.title = title;
+        this.writer = writer;
+    }
+}
